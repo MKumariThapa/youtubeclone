8f740761e9c1d033e0d84765e9c7786aa527ce56
mMay03 <yamkumari.thapa03@gmail.com>
2023-01-12 15:09:51 +0545

    routes fixed

--- src/App.js
+++ src/App.js
@@ -8,23 +8,23 @@ import VideoDetails from "./components/VideoDetails";
 import { AppContext } from "./context/contextApi";
 
 const App = () => {
-    return (
-        <AppContext>
-            <BrowserRouter>
-                <div className="flex flex-col h-full">
-                    <Header />
-                    <Routes>
-                        <Route path="/" exact element={<Feed />} />
-                        <Route
-                            path="/searchResult/:searchQuery"
-                            element={<SearchResult />}
-                        />
-                        <Route path="/video/:id" element={<VideoDetails />} />
-                    </Routes>
-                </div>
-            </BrowserRouter>
-        </AppContext>
-    );
+  return (
+    <AppContext>
+      <BrowserRouter>
+        <div className="flex flex-col h-full">
+          <Header />
+          <Routes>
+            <Route path="/youtubeclone" exact element={<Feed />} />
+            <Route
+              path="/searchResult/:searchQuery"
+              element={<SearchResult />}
+            />
+            <Route path="/video/:id" element={<VideoDetails />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </AppContext>
+  );
 };
 
 export default App;
